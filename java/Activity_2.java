42b01d17ef5b3143f6ad935361a609dd77a7b171
kulki-adi22 <67149382+kulki-adi22@users.noreply.github.com>
2022-01-09 17:01:52 +0530

    Update Activity_2.java

--- java/Activity_2.java
+++ java/Activity_2.java
@@ -1,12 +1,29 @@
 package com.example.mad_app1;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,11 +31,16 @@ import java.util.List;
 public class Activity2 extends AppCompatActivity {
     private Spinner subjects;
     String selectedSubject;
-
+    FirebaseFirestore db;
+    TextView t1;
+    Button b1;
+    static final String TAG="Read Data Activity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        b1 = findViewById(R.id.btntemp);
+        db = FirebaseFirestore.getInstance();
         subjects=findViewById(R.id.spinner_subjects);
         List<String> sub_list = new ArrayList<String>();
         sub_list.add(0,"Choose a subject");
@@ -44,5 +66,44 @@ public class Activity2 extends AppCompatActivity {
             }
         });
 
+        QuestionSet q = new QuestionSet();
+        final String[] str = new String[1];
+//        DocumentReference documentReference = db.collection("Questions").document();
+//        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+//            @Override
+//            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+//                str[0] = ((String) value.get("Questions"));
+//            }
+//        });
+        List<String> q_list = new ArrayList<String>();
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.collection("Questions")
+                        .whereEqualTo("subject","Computer Networks-1")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful())
+                                {
+                                    Toast.makeText(Activity2.this,"read Succesful",Toast.LENGTH_SHORT).show();
+                                    for(QueryDocumentSnapshot document : task.getResult()){
+                                        Log.d(TAG,document.getId()+"=>"+document.getData());
+                                        q_list.add(document.getData().toString());
+                                    }
+
+                                }
+                            }
+                        });
+                b1.setVisibility(View.INVISIBLE);
+            }
+        });
+        
+
+
+
     }
+
+
 }
