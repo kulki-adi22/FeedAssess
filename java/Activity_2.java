8e8146810d5b06a6640438174698b7700283563d
kulki-adi22 <67149382+kulki-adi22@users.noreply.github.com>
2022-01-13 02:42:58 +0530

    Update Activity_2.java

--- java/Activity_2.java
+++ java/Activity_2.java
@@ -4,12 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +19,7 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -30,27 +33,40 @@ import java.util.List;
 
 public class Activity2 extends AppCompatActivity {
     private Spinner subjects;
-    String selectedSubject;
+    String selectedSubject="";
     FirebaseFirestore db;
-    TextView t1;
+    TextView usnText;
     Button b1;
-    static final String TAG="Read Data Activity";
+    TextView q1, q2, q3, q4, q5;
+    ScrollView qList;
+    List<Question> q_list;
+    static final String TAG = "Read Data Activity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
-        b1 = findViewById(R.id.btntemp);
+        b1 = findViewById(R.id.btnTemp);
         db = FirebaseFirestore.getInstance();
-        subjects=findViewById(R.id.spinner_subjects);
+        subjects = findViewById(R.id.spinner_subjects);
+        q1 = findViewById(R.id.ques1);
+        q2 = findViewById(R.id.ques2);
+        q3 = findViewById(R.id.ques3);
+        q4 = findViewById(R.id.ques4);
+        q5 = findViewById(R.id.ques5);
+        qList = findViewById(R.id.ques_list);
+        usnText = findViewById(R.id.usnInput);
+        qList = findViewById(R.id.ques_list);
         List<String> sub_list = new ArrayList<String>();
-        sub_list.add(0,"Choose a subject");
+        ArrayList<String> questions = new ArrayList<String>();
+        sub_list.add(0, "Choose a subject");
         sub_list.add("Computer Networks-1");
         sub_list.add("Mobile Application Development");
         sub_list.add("Data Structures and Algorithms");
         sub_list.add("Operating Systems");
         sub_list.add("Database Management System");
         sub_list.add("Computer Networks-2");
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,sub_list);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, sub_list);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         subjects.setAdapter(arrayAdapter);
         subjects.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -60,50 +76,55 @@ public class Activity2 extends AppCompatActivity {
                 if (!selected.equals("Choose a subject"))
                     selectedSubject = parent.getItemAtPosition(position).toString();
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
             }
         });
-
-        QuestionSet q = new QuestionSet();
-        final String[] str = new String[1];
-//        DocumentReference documentReference = db.collection("Questions").document();
-//        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-//            @Override
-//            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
-//                str[0] = ((String) value.get("Questions"));
-//            }
-//        });
-        List<String> q_list = new ArrayList<String>();
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                db.collection("Questions")
-                        .whereEqualTo("subject","Computer Networks-1")
-                        .get()
-                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                if (task.isSuccessful())
-                                {
-                                    Toast.makeText(Activity2.this,"read Succesful",Toast.LENGTH_SHORT).show();
-                                    for(QueryDocumentSnapshot document : task.getResult()){
-                                        Log.d(TAG,document.getId()+"=>"+document.getData());
-                                        q_list.add(document.getData().toString());
-                                    }
-
-                                }
-                            }
-                        });
+                subjects.setVisibility(View.INVISIBLE);
                 b1.setVisibility(View.INVISIBLE);
+                usnText.setVisibility(View.INVISIBLE);
+                DocumentReference docRef = db.collection("Questions").document(selectedSubject);
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            String qu1 = document.get("q1").toString();
+                            String qu2 = document.get("q2").toString();
+                            String qu3 = document.get("q3").toString();
+                            String qu4 = document.get("q4").toString();
+                            String qu5 = document.get("q5").toString();
+                            setQuestions(qu1,qu2,qu3,qu4,qu5);
+                            q1.setText(qu1);
+                            if (document.exists()) {
+                                Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                            } else {
+                                Log.d(TAG, "No such document");
+                            }
+                        } else {
+                            Log.d(TAG, "get failed with ", task.getException());
+                        }
+                    }
+                });
+
+                qList.setVisibility(View.VISIBLE);
             }
         });
-        
-
-
 
     }
 
+    private void setQuestions(String qu1, String qu2, String qu3, String qu4, String qu5) {
+        q1.setText(qu1);
+        q2.setText(qu2);
+        q3.setText(qu3);
+        q4.setText(qu4);
+        q5.setText(qu5);
 
+    }
+    
 }
