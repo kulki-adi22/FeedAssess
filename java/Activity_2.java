c04b684975f6b71a601a2cb718240dd306d97752
kulki-adi22 <67149382+kulki-adi22@users.noreply.github.com>
2022-01-19 02:19:48 +0530

    Update Activity_2.java

--- java/Activity_2.java
+++ java/Activity_2.java
@@ -11,6 +11,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -19,6 +21,7 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -34,20 +37,24 @@ import java.util.List;
 public class Activity2 extends AppCompatActivity {
     private Spinner subjects;
     String selectedSubject="";
-    FirebaseFirestore db;
+    FirebaseFirestore db,dbpush;
     TextView usnText;
-    Button b1;
+    Button b1,submit;
     TextView q1, q2, q3, q4, q5;
     ScrollView qList;
     List<Question> q_list;
     static final String TAG = "Read Data Activity";
-
+    RadioGroup r1,r2,r3,r4,r5;
+    RadioButton rb1,rb2,rb3,rb4,rb5;
+    Responses responses;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        responses = new Responses();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
         b1 = findViewById(R.id.btnTemp);
         db = FirebaseFirestore.getInstance();
+        dbpush = FirebaseFirestore.getInstance();
         subjects = findViewById(R.id.spinner_subjects);
         q1 = findViewById(R.id.ques1);
         q2 = findViewById(R.id.ques2);
@@ -57,6 +64,12 @@ public class Activity2 extends AppCompatActivity {
         qList = findViewById(R.id.ques_list);
         usnText = findViewById(R.id.usnInput);
         qList = findViewById(R.id.ques_list);
+        submit=findViewById(R.id.submitBtn);
+        r1 = findViewById(R.id.q1);
+        r2 = findViewById(R.id.q2);
+        r3 = findViewById(R.id.q3);
+        r4 = findViewById(R.id.q4);
+        r5 = findViewById(R.id.q5);
         List<String> sub_list = new ArrayList<String>();
         ArrayList<String> questions = new ArrayList<String>();
         sub_list.add(0, "Choose a subject");
@@ -114,6 +127,51 @@ public class Activity2 extends AppCompatActivity {
 
             }
         });
+
+        submit= findViewById(R.id.submitBtn);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity2.this,MainActivity.class);
+                List<String> resList = new ArrayList<String >();
+                rb1=(RadioButton) findViewById(r1.getCheckedRadioButtonId());
+                resList.add(rb1.getText().toString());
+                rb2=(RadioButton) findViewById(r2.getCheckedRadioButtonId());
+                resList.add(rb2.getText().toString());
+                rb3=(RadioButton) findViewById(r3.getCheckedRadioButtonId());
+                resList.add(rb3.getText().toString());
+                rb4=(RadioButton) findViewById(r4.getCheckedRadioButtonId());
+                resList.add(rb4.getText().toString());
+                rb5=(RadioButton) findViewById(r5.getCheckedRadioButtonId());
+                resList.add(rb5.getText().toString());
+                String usn =usnText.getText().toString();
+                addResponses(usn,resList,selectedSubject);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void addResponses(String usnText, List<String> resList, String selectedSubject) {
+        responses.setRes1(resList.get(0));
+        responses.setRes2(resList.get(1));
+        responses.setRes3(resList.get(2));
+        responses.setRes4(resList.get(3));
+        responses.setRes5(resList.get(4));
+        responses.setSubject(selectedSubject);
+        CollectionReference response = dbpush.collection("Responses");
+        response.document(usnText.toString()).set(responses).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Toast.makeText(getApplicationContext(),"Responses stored successfully",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intent = new Intent(Activity2.this,MainActivity.class);
 
     }
 
