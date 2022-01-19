0db6ea89d8b5cd01176b6376aad1f942b0a2a277
kulki-adi22 <67149382+kulki-adi22@users.noreply.github.com>
2022-01-20 02:39:02 +0530

    Update MainActivity.java

--- java/MainActivity.java
+++ java/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btn1,btn2;
@@ -25,7 +26,8 @@ public class MainActivity extends AppCompatActivity {
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,Activity3.class);
+//                Toast.makeText(getApplicationContext(), "Admin clicked", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this,adminverify.class);
                 startActivity(intent);
             }
         });
