4d46173b8190c5d6cf6f4408971e5cf76e119fe5
kulki-adi22 <67149382+kulki-adi22@users.noreply.github.com>
2022-01-20 01:27:00 +0530

    Update Activity_2.java

--- java/Activity_2.java
+++ java/Activity_2.java
@@ -1,7 +1,6 @@
 package com.example.mad_app1;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -22,12 +21,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -36,17 +32,18 @@ import java.util.List;
 
 public class Activity2 extends AppCompatActivity {
     private Spinner subjects;
-    String selectedSubject="";
-    FirebaseFirestore db,dbpush;
+    String selectedSubject = "";
+    FirebaseFirestore db, dbpush;
     TextView usnText;
-    Button b1,submit;
+    Button b1, submit;
     TextView q1, q2, q3, q4, q5;
     ScrollView qList;
     List<Question> q_list;
     static final String TAG = "Read Data Activity";
-    RadioGroup r1,r2,r3,r4,r5;
-    RadioButton rb1,rb2,rb3,rb4,rb5;
+    RadioGroup r1, r2, r3, r4, r5;
+    RadioButton rb1, rb2, rb3, rb4, rb5;
     Responses responses;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         responses = new Responses();
@@ -64,7 +61,7 @@ public class Activity2 extends AppCompatActivity {
         qList = findViewById(R.id.ques_list);
         usnText = findViewById(R.id.usnInput);
         qList = findViewById(R.id.ques_list);
-        submit=findViewById(R.id.submitBtn);
+        submit = findViewById(R.id.submitBtn);
         r1 = findViewById(R.id.q1);
         r2 = findViewById(R.id.q2);
         r3 = findViewById(R.id.q3);
@@ -95,60 +92,100 @@ public class Activity2 extends AppCompatActivity {
 
             }
         });
+        String msg = "Subject feedback not added! Please contact admin";
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                subjects.setVisibility(View.INVISIBLE);
-                b1.setVisibility(View.INVISIBLE);
-                usnText.setVisibility(View.INVISIBLE);
-                DocumentReference docRef = db.collection("Questions").document(selectedSubject);
-                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                        if (task.isSuccessful()) {
-                            DocumentSnapshot document = task.getResult();
-                            String qu1 = document.get("q1").toString();
-                            String qu2 = document.get("q2").toString();
-                            String qu3 = document.get("q3").toString();
-                            String qu4 = document.get("q4").toString();
-                            String qu5 = document.get("q5").toString();
-                            setQuestions(qu1,qu2,qu3,qu4,qu5);
-                            q1.setText(qu1);
-                            if (document.exists()) {
-                                Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-                            } else {
-                                Log.d(TAG, "No such document");
-                            }
-                        } else {
-                            Log.d(TAG, "get failed with ", task.getException());
-                        }
-                    }
-                });
+                if (usnText.getText().toString().equals(""))
+                    Toast.makeText(getApplicationContext(), "Please enter usn", Toast.LENGTH_SHORT).show();
+                else {
+                    if (subjects.getSelectedItem().toString().equals("Choose a subject"))
+                        Toast.makeText(getApplicationContext(), "Please choose subject", Toast.LENGTH_SHORT).show();
+                    else
+                        getData();
+                }
 
             }
         });
-
-        submit= findViewById(R.id.submitBtn);
+        Intent intent = new Intent(Activity2.this, MainActivity.class);
+        submit = findViewById(R.id.submitBtn);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Activity2.this,MainActivity.class);
-                List<String> resList = new ArrayList<String >();
-                rb1=(RadioButton) findViewById(r1.getCheckedRadioButtonId());
+                List<String> resList = new ArrayList<String>();
+                rb1 = (RadioButton) findViewById(r1.getCheckedRadioButtonId());
                 resList.add(rb1.getText().toString());
-                rb2=(RadioButton) findViewById(r2.getCheckedRadioButtonId());
+                rb2 = (RadioButton) findViewById(r2.getCheckedRadioButtonId());
                 resList.add(rb2.getText().toString());
-                rb3=(RadioButton) findViewById(r3.getCheckedRadioButtonId());
+                rb3 = (RadioButton) findViewById(r3.getCheckedRadioButtonId());
                 resList.add(rb3.getText().toString());
-                rb4=(RadioButton) findViewById(r4.getCheckedRadioButtonId());
+                rb4 = (RadioButton) findViewById(r4.getCheckedRadioButtonId());
                 resList.add(rb4.getText().toString());
-                rb5=(RadioButton) findViewById(r5.getCheckedRadioButtonId());
+                rb5 = (RadioButton) findViewById(r5.getCheckedRadioButtonId());
                 resList.add(rb5.getText().toString());
-                String usn =usnText.getText().toString();
-                addResponses(usn,resList,selectedSubject);
+                String usn = usnText.getText().toString();
+                addResponses(usn, resList, selectedSubject);
+                Toast.makeText(getApplicationContext(), "Responses stored successfully", Toast.LENGTH_SHORT).show();
+
                 startActivity(intent);
             }
         });
+    }
+
+    private void getData() {
+        subjects.setVisibility(View.INVISIBLE);
+        b1.setVisibility(View.INVISIBLE);
+        usnText.setVisibility(View.INVISIBLE);
+        DocumentReference docRef = db.collection("Questions").document(selectedSubject);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    String qu1 = document.get("q1").toString();
+                    String qu2 = document.get("q2").toString();
+                    String qu3 = document.get("q3").toString();
+                    String qu4 = document.get("q4").toString();
+                    String qu5 = document.get("q5").toString();
+                    setQuestions(qu1, qu2, qu3, qu4, qu5);
+                    q1.setText(qu1);
+                    if (document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+    }
+
+    private int checkSubject() {
+        int flag = 0;
+        String questions = db.collection("Questions").getId();
+        String temp = "";
+        List<String> list = new ArrayList<>();
+        db.collection("Questions").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        list.add(document.getId());
+                    }
+                    Log.d(TAG, list.toString());
+                } else {
+                    Log.d(TAG, "Error getting documents: ", task.getException());
+                }
+            }
+        });
+        for (int i = 0; i < list.size(); i++) {
+            temp = list.get(i);
+            if (selectedSubject.equals(temp))
+                flag = 1;
+        }
+        Log.d(TAG, String.valueOf(flag));
+        return flag;
     }
 
     private void addResponses(String usnText, List<String> resList, String selectedSubject) {
@@ -162,7 +199,7 @@ public class Activity2 extends AppCompatActivity {
         response.document(usnText.toString()).set(responses).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void unused) {
-                Toast.makeText(getApplicationContext(),"Responses stored successfully",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Responses stored successfully", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -171,7 +208,7 @@ public class Activity2 extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        Intent intent = new Intent(Activity2.this,MainActivity.class);
+        Intent intent = new Intent(Activity2.this, MainActivity.class);
 
     }
 
