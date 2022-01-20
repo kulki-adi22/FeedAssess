eaa787abd913940be658223fd8119b58e1ad1edd
kulki-adi22 <67149382+kulki-adi22@users.noreply.github.com>
2022-01-21 01:27:30 +0530

    Create DBOps.java

---
+++ java/DBOps.java
@@ -0,0 +1,59 @@
+package com.example.mad_app1;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class DBOps {
+//    String subject;
+    String q1,q2,q3,q4,q5;
+    DBOps(){}
+
+//    public String getSubject() {
+//        return subject;
+//    }
+//
+//    public void setSubject(String subject) {
+//        this.subject = subject;
+//    }
+
+    public String getQ1() {
+        return q1;
+    }
+
+    public void setQ1(String q1) {
+        this.q1 = q1;
+    }
+
+    public String getQ2() {
+        return q2;
+    }
+
+    public void setQ2(String q2) {
+        this.q2 = q2;
+    }
+
+    public String getQ3() {
+        return q3;
+    }
+
+    public void setQ3(String q3) {
+        this.q3 = q3;
+    }
+
+    public String getQ4() {
+        return q4;
+    }
+
+    public void setQ4(String q4) {
+        this.q4 = q4;
+    }
+
+    public String getQ5() {
+        return q5;
+    }
+
+    public void setQ5(String q5) {
+        this.q5 = q5;
+    }
+}
